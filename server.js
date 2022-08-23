0652b06d695c5269208d40da0b92031f954829fe
Konomaster <thomasdovaletec@gmail.com>
2022-08-23 10:16:00 -0300

    vai?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults({static: "./dist"})
+const middlewares = jsonServer.defaults({static: "./dist/delta-app-thomas"})
 
 server.use(middlewares)
 server.use(router)
